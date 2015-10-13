5c34123e9d00a175040ae5266ad43dfef894f863
Travis Stebbins <stebbins.travis@gmail.com>
2015-10-13 12:19:32 -0500

    Migrate existing game data into repository

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public bool centered = true;
+	public int scrollDistance = 5;
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (centered)
+			transform.position = new Vector3 (player.transform.position.x, transform.position.y, transform.position.z);
+		else {
+			if (player.transform.position.x > transform.position.x + (this.GetComponent<Camera>().orthographicSize * (16.0/9.0) - scrollDistance)) {
+				transform.position = new Vector3 (player.transform.position.x - (float)(this.GetComponent<Camera>().orthographicSize * (16.0/9.0) - scrollDistance), transform.position.y, transform.position.z);
+			}
+			else if (player.transform.position.x < transform.position.x - (this.GetComponent<Camera>().orthographicSize * (16.0/9.0)) + scrollDistance) {
+				transform.position = new Vector3 (player.transform.position.x + (float)(this.GetComponent<Camera>().orthographicSize * (16.0/9.0) - scrollDistance), transform.position.y, transform.position.z);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 1f;
+	public float jumpForce = 1f;
+
+	private Rigidbody2D rb;
+	private bool grounded = false;
+	private bool doubleJump = false;
+	private Vector3 prevPos;
+	private bool doubleGrav = false;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis ("Horizontal");
+		if (grounded)
+			transform.Translate (new Vector3 (horizontal * speed, 0, 0) * Time.deltaTime);
+		if (Input.GetKeyDown (KeyCode.Space) && (grounded || !doubleJump)) {
+			rb.AddForce(new Vector2(0, jumpForce));
+			if (grounded) {
+				grounded = false;
+				rb.velocity = (transform.position - prevPos) / Time.deltaTime;
+			}
+			else
+				doubleJump = true;
+		}
+		if (rb.velocity.y < 0 && !doubleGrav) {
+			Physics2D.gravity = Physics2D.gravity * 2.0f;
+			doubleGrav = true;
+		}
+		prevPos = transform.position;
+	}
+
+	void FixedUpdate () {
+		if (rb.velocity.y < 0 && !doubleGrav) {
+			Physics2D.gravity = Physics2D.gravity * 2.0f;
+			doubleGrav = true;
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		if (collision.gameObject.CompareTag("Ground")) {
+			grounded = true;
+			doubleJump = false;
+			if (doubleGrav) {
+				Physics2D.gravity = Physics2D.gravity / 2.0f;
+				doubleGrav = false;
+			}
+		}
+	}
+}
