2b1682e3569c7d960f7a1d038655ee3544d12103
Travis Stebbins <stebbins.travis@gmail.com>
2015-11-02 22:56:47 -0600

    Add main menu and clean up level 1 hierarchy

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	public void Play () {
+		Application.LoadLevel ("Level1");
+	}
+
+	public void LevelSelect () {
+
+	}
+
+	public void Options () {
+
+	}
+
+	public void Quit () {
+		Application.Quit ();
+	}
+
+}
