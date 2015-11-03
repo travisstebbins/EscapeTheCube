494f77fe10790ee2be8603442558447dc6b74e93
Travis Stebbins <stebbins.travis@gmail.com>
2015-11-02 23:17:08 -0600

    Continue implementing main menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -19,4 +19,18 @@ public class MenuManager : MonoBehaviour {
 		Application.Quit ();
 	}
 
+	public void LoadLevel (int l) {
+		switch (l) {
+		case 0:
+			Application.LoadLevel ("Tutorial");
+			break;
+		case 1:
+			Application.LoadLevel ("Level1");
+			break;
+		case 2:
+			Application.LoadLevel ("Level2");
+			break;
+		}
+	}
+
 }
